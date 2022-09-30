4e7a5e8d99e15c8d99dc1fedf53256ce5ac854e7
Andy <achim.andrei97@gmail.com>
2022-09-30 17:07:39 +0300

    data types

---
+++ assignments/assignments.js
@@ -0,0 +1,15 @@
+console.log("kkpishu");
+
+let country = "Romania";
+let continent = "Europe";
+let population = 19;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- fundamentals-1/script.js
+++ fundamentals-1/script.js
@@ -1,1 +1,35 @@
-console.log('hello');
+// console.log("hello");
+// console.log(40 + 5 + 100092);
+
+// console.log("Andy");
+
+// let firstName = "Andy";
+// console.log(firstName);
+
+// let PI = 3.1415;
+
+// let myFirstJob = "NDT inspector";
+// let myCurrentJob = "JavaScript Dev";
+
+// console.log(myCurrentJob);
+
+true;
+console.log(true);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+let nr = 12;
+console.log(typeof 12);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof "Andy");
+
+let andy;
+console.log(andy);
+
+javascriptIsFun = "Yeah";
+
+console.log(javascriptIsFun);
+
+console.log(typeof null);

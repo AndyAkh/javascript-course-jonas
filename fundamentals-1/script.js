85d08bc70d26e87c573cc0a148a25d87f2244610
Andy <achim.andrei97@gmail.com>
2022-10-02 18:56:04 +0300

    add challenges folder

--- fundamentals-1/script.js
+++ fundamentals-1/script.js
@@ -13,23 +13,66 @@
 
 // console.log(myCurrentJob);
 
-true;
-console.log(true);
+// true;
+// console.log(true);
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-let nr = 12;
-console.log(typeof 12);
+// let nr = 12;
+// console.log(typeof 12);
 
-console.log(typeof javascriptIsFun);
-console.log(typeof "Andy");
+// console.log(typeof javascriptIsFun);
+// console.log(typeof "Andy");
 
-let andy;
-console.log(andy);
+// let andy;
+// console.log(andy);
 
-javascriptIsFun = "Yeah";
+// javascriptIsFun = "Yeah";
 
-console.log(javascriptIsFun);
+// console.log(javascriptIsFun);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+// let age = 25;
+// age = 26;
+
+// const birthYear = 1997;
+// birthYear = 1999;
+
+// Math operators
+// const now = 2037;
+// const ageAndy = now - 1997;
+// const ageAlecsandra = now - 1999;
+
+// console.log(ageAndy, ageAlecsandra);
+// console.log(ageAndy * 2, ageAndy / 2, 2 ** 11);
+
+// const firstName = "Andrei";
+// const lastName = "Achim";
+// console.log(firstName + " " + lastName);
+
+// // Assignment operators
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// x--;
+// console.log(x);
+
+// // Comparison operators
+// console.log(ageAndy > ageAlecsandra);
+// console.log(ageAlecsandra >= 39);
+
+// const isFullAge = ageAlecsandra >= 18;
+
+const now = 2037;
+const ageAndy = now - 1997;
+const ageAlecsandra = now - 1999;
+
+console.log(now - 1997 > now - 1999);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);

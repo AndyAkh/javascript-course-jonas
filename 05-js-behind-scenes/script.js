abf6f45ff6e9111d8dbc09bdb84cdfa34ff2c5ea
Andy <achim.andrei97@gmail.com>
2022-10-27 17:55:08 +0300

    primitive vs reference types; copying objects

--- 05-js-behind-scenes/script.js
+++ 05-js-behind-scenes/script.js
@@ -36,10 +36,11 @@ calcAge(1997);
 */
 
 // Hoisting with variables
-console.log(me);
+// console.log(me);
 // console.log(job);
 // console.log(age);
 
+/*
 var me = 'Andy';
 let job = 'developer';
 const age = 29;
@@ -69,3 +70,148 @@ function deleteShoppingCart() {
 }
 
 var x = 144;
+*/
+
+/*
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAge(9999);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1772);
+
+const andy = {
+  name: 'Andy',
+  year: 1997,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+andy.calcAge();
+
+const iulia = {
+  year: 1999,
+};
+
+iulia.calcAge = andy.calcAge;
+
+iulia.calcAge();
+*/
+
+// var firstName = 'Ionel';
+
+// const andy = {
+//   firstName: 'Andy',
+//   year: 1997,
+//   calcAge: function () {
+//     // console.log(this);
+//     console.log(2037 - this.year);
+
+//     const isMillenial = () => {
+//       console.log(this.year >= 1981 && this.year <= 1996);
+//     };
+//     isMillenial();
+//   },
+
+//   /*
+//   const self = this; // self or that
+//     const isMillenial = function () {
+//       console.log(self.year >= 1981 && self.year <= 1996);
+//     };
+//     isMillenial();
+//   },
+//   */
+//   greet: () => {
+//     console.log(this);
+//     console.log(`Hello ${this.firstName}!`);
+//   },
+// };
+
+// andy.greet();
+// andy.calcAge();
+
+// // Arguments keyword
+// const addExpr = function (a, b) {
+//   console.log(arguments);
+//   console.log(a + b);
+//   return a + b;
+// };
+
+// addExpr(2, 4);
+// addExpr(5, 10, 8, 55);
+
+// const addArrow = () => {
+//   console.log(arguments);
+//   return a + b;
+// };
+
+// addArrow(1, 2);
+
+/*
+let age = 30;
+let oldAge = age;
+age = 31;
+
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  firstName: 'Andy',
+  age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+
+console.log(`Friend:`, friend);
+console.log(`Me`, me);
+*/
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+
+lastName = 'Davis';
+
+console.log(lastName);
+console.log(oldLastName);
+
+// Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Ronaldo';
+
+console.log(jessica);
+console.log(marriedJessica);
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Mbappe', 'Hagi'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2); // shallow copy, not deep clone
+jessicaCopy.lastName = 'Griezmann';
+jessicaCopy.family.push('Dica');
+jessicaCopy.family.push('Sapunaru');
+
+console.log(jessica2);
+console.log(jessicaCopy);
